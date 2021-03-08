a9157e55e9647676967d5b8ff51651b794dbac09
Marfusha-91 <71018868+Marfusha-91@users.noreply.github.com>
2021-03-08 22:57:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (array) { 
+    if ( !array  || array.length === 0 )  return 0; 
+    else 
+   return  Math.min(...array); 
+ } 
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max (array) { 
+    if ( !array  || array.length === 0 )  return 0; 
+    else 
+   return  Math.max(...array); 
+ } 
+
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( !array  || array.length === 0 )  return 0;
+   else
+    const reduser = (a,c) => (a+c) / array.length
+    return arr.reduce(reduser, 0) 
 }
